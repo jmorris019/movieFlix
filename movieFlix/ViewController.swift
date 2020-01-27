7ea8fe67b9bc06d29c95e84d348e60db34bc0807
James Morris <morri398@purdue.edu>
2020-01-26 23:05:51 -0500

    Initial Commit

---
+++ movieFlix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  movieFlix
+//
+//  Created by James Morris on 1/26/20.
+//  Copyright © 2020 James Morris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
